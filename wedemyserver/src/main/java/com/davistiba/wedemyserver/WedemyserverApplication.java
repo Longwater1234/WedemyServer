e8da8f36e0536c060442a15fbf07cb493efaacc8
Davis Tibbz <davisinyo@gmail.com>
2021-06-06 08:50:39 +0800

    created the springboot app

---
+++ wedemyserver/src/main/java/com/davistiba/wedemyserver/WedemyserverApplication.java
@@ -0,0 +1,13 @@
+package com.davistiba.wedemyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WedemyserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WedemyserverApplication.class, args);
+	}
+
+}
---
+++ wedemyserver/src/test/java/com/davistiba/wedemyserver/WedemyserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davistiba.wedemyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WedemyserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
