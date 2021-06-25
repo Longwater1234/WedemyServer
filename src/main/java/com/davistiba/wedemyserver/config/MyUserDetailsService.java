04b0d001fa5efa2bab1b01fe2967b9a07cda44bf
Davis Tibbz <davisinyo@gmail.com>
2021-06-25 21:59:34 +0800

    finally can login, and recieve cookie.

--- src/main/java/com/davistiba/wedemyserver/config/MyUserDetailsService.java
+++ src/main/java/com/davistiba/wedemyserver/config/MyUserDetailsService.java
@@ -15,6 +15,7 @@ public class MyUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByEmail(username).orElseThrow();
+        return userRepository.findByEmail(username).orElseThrow(() ->
+                new UsernameNotFoundException("Wrong email or password"));
     }
 }
