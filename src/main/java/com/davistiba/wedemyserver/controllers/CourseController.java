f262efcc9c34750e54a511b592b93dcc5eed32e7
Davis Tibbz <davisinyo@gmail.com>
2021-10-05 18:54:36 +0800

    Added service for querying Courses by Category.

--- src/main/java/com/davistiba/wedemyserver/controllers/CourseController.java
+++ src/main/java/com/davistiba/wedemyserver/controllers/CourseController.java
@@ -1,7 +1,10 @@
 package com.davistiba.wedemyserver.controllers;
 
+import com.davistiba.wedemyserver.dto.CategoryDTO;
 import com.davistiba.wedemyserver.models.Course;
 import com.davistiba.wedemyserver.repository.CourseRepository;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -10,48 +13,64 @@ import org.springframework.web.server.ResponseStatusException;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/courses")
 public class CourseController {
 
-    @Autowired
-    CourseRepository courseRepository;
+	@Autowired
+	CourseRepository courseRepository;
 
-    @GetMapping(path = "/id/{id}")
-    public Course getCoursebyId(@PathVariable(value = "id") @NotNull Integer id) {
-        try {
-            return courseRepository.findById(id).orElseThrow();
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No course of id " + id);
-        }
-    }
+	@Autowired
+	ModelMapper mapper;
 
-    @GetMapping(path = "/cat/{category}")
-    @ResponseStatus(value = HttpStatus.OK)
-    public List<Course> getCoursesByCategory(@PathVariable(value = "category")
-                                             @NotBlank String category) {
-        var courseList = courseRepository.getCoursesByCategoryEquals(category);
-        if (courseList.isEmpty())
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No results for category " + category);
-        return courseList;
-    }
+	@GetMapping(path = "/id/{id}")
+	public Course getCoursebyId(@PathVariable(value = "id") @NotNull Integer id) {
+		try {
+			return courseRepository.findById(id).orElseThrow();
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No course of id " + id);
+		}
+	}
 
-    @GetMapping(path = "/all")
-    @ResponseStatus(value = HttpStatus.OK)
-    public List<Course> getAllTopCourses() {
-        return courseRepository.getTop6ByRatingGreaterThanEqual(4.5);
-    }
+	@GetMapping(path = "/cat/{category}")
+	@ResponseStatus(value = HttpStatus.OK)
+	public List<Course> getCoursesByCategory(@PathVariable(value = "category") @NotBlank String category) {
+		var courseList = courseRepository.getCoursesByCategoryEquals(category);
+		if (courseList.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No results for category " + category);
+		return courseList;
+	}
 
-    @GetMapping(path = "/search")
-    public List<Course> searchForCourse(@RequestParam(value = "title", defaultValue = "")
-                                        @NotBlank String title) {
+	@GetMapping(path = "/top")
+	@ResponseStatus(value = HttpStatus.OK)
+	public List<Course> getAllTopCourses() {
+		return courseRepository.getTop6ByRatingGreaterThanEqual(4.5);
+	}
 
-        if (title.length() < 4) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Query too short");
-        var searchResults = courseRepository.getCoursesByTitleContaining(title);
-        if (searchResults.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No results for " + title);
-        }
-        return searchResults;
-    }
+	@GetMapping(path = "/categories")
+	@ResponseStatus(value = HttpStatus.OK)
+	public List<CategoryDTO> getCategoryListDistinct() {
+		try {
+			var courseList = courseRepository.getAllDistinctCategories();
+			return courseList.stream().map(item -> mapper.map(item, CategoryDTO.class)).collect(Collectors.toList());
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+		}
+
+	}
+
+	@GetMapping(path = "/search")
+	public List<Course> searchForCourse(@RequestParam(value = "title", defaultValue = "") @NotBlank String title) {
+
+		if (title.length() < 4)
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Query too short");
+		var searchResults = courseRepository.getCoursesByTitleContaining(title);
+		if (searchResults.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No results for " + title);
+		}
+		return searchResults;
+	}
 }
--- src/main/java/com/davistiba/wedemyserver/repository/CourseRepository.java
+++ src/main/java/com/davistiba/wedemyserver/repository/CourseRepository.java
@@ -1,6 +1,8 @@
 package com.davistiba.wedemyserver.repository;
 
 import com.davistiba.wedemyserver.models.Course;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import javax.validation.constraints.NotBlank;
@@ -8,9 +10,12 @@ import java.util.List;
 
 public interface CourseRepository extends CrudRepository<Course, Integer> {
 
-    List<Course> getCoursesByCategoryEquals(@NotBlank String category);
+	List<Course> getCoursesByCategoryEquals(@NotBlank String category);
 
-    List<Course> getTop6ByRatingGreaterThanEqual(double rating);
+	List<Course> getTop6ByRatingGreaterThanEqual(double rating);
 
-    List<Course> getCoursesByTitleContaining(@NotBlank String title);
+	List<Course> getCoursesByTitleContaining(@NotBlank String title);
+
+	@Query(value = "SELECT * FROM courses GROUP BY category", nativeQuery = true)
+	List<Course> getAllDistinctCategories();
 }
