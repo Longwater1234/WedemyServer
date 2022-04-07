c942e3afb15b959d4b2e01b409696eca01642e58
Davis Tibbz <33986524+Longwater1234@users.noreply.github.com>
2022-04-07 23:20:17 +0800

    USELESS

--- src/main/java/com/davistiba/wedemyserver/repository/CartRepository.java
+++ src/main/java/com/davistiba/wedemyserver/repository/CartRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
 import java.util.Collection;
 
 @Repository
@@ -15,18 +14,11 @@ public interface CartRepository extends CrudRepository<Cart, Integer> {
 
     @Modifying
     @Transactional
-    @Query(value = "INSERT INTO cart(created_at, course_id, user_id, price) VALUES (UTC_TIMESTAMP(), ?, ?, ?)", nativeQuery = true)
-    Integer addToCartCustom(Integer courseId, Integer userId, BigDecimal price);
-
-    @Modifying
-    @Transactional
     @Query(value = "DELETE FROM Cart c where c.user.id = ?1 and c.course.id in ?2")
     Integer deleteAllByUserIdAndCoursesIn(Integer userId, Collection<Integer> courseId);
 
-
     @Query("SELECT (count(c) > 0) from Cart c where c.course.id = ?1 and c.user.id = ?2")
     boolean checkIfCourseInCart(Integer courseId, Integer userId);
-
 
     @Query(value = "SELECT COUNT(c) FROM Cart c WHERE c.user.id = ?1")
     Integer countCartByUserIdEquals(Integer userId);
