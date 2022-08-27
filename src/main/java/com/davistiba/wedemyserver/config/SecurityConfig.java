0541d14854feec3e49f39c58f861e2fb3556e016
Davis Tibbz <davisinyo@gmail.com>
2022-08-27 09:38:20 +0800

    removed Hcaptcha

--- src/main/java/com/davistiba/wedemyserver/config/SecurityConfig.java
+++ src/main/java/com/davistiba/wedemyserver/config/SecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
@@ -49,15 +48,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private CustomOauthSuccessHandler successHandler;
 
-    @Autowired
-    private CaptchaFilter captchaFilter;
-
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().httpBasic()
-                .and().addFilterAt(captchaFilter, UsernamePasswordAuthenticationFilter.class)
-                .oauth2Login().userInfoEndpoint().oidcUserService(googleOauthService)
+                .and().oauth2Login().userInfoEndpoint().oidcUserService(googleOauthService)
                 .and().successHandler(successHandler)
                 .and().authorizeRequests()
                 .antMatchers("/index.html", "/", "/auth/**", "/favicon.ico", "/login/**").permitAll()
