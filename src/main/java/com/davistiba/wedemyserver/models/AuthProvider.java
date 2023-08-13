3ba47cc7a1ba60553ad1ebb5ada0c1eac813a81e
Davis Tibbz <davisinyo@gmail.com>
2023-08-13 22:35:19 +0800

    CREATED docs endpoint

--- src/main/java/com/davistiba/wedemyserver/models/AuthProvider.java
+++
@@ -1,9 +0,0 @@
-package com.davistiba.wedemyserver.models;
-
-/**
- * [NOT A TABLE]
- * What method Student used to register
- */
-public enum AuthProvider {
-    LOCAL, GOOGLE
-}
--- src/main/java/com/davistiba/wedemyserver/models/UserRole.java
+++
@@ -1,9 +0,0 @@
-package com.davistiba.wedemyserver.models;
-
-/**
- * [NOT A TABLE]
- * Stored in column inside `users` table
- */
-public enum UserRole {
-    ROLE_STUDENT, ROLE_ADMIN
-}
