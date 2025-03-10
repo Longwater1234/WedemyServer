94d0906de12cb3bb14dcd53ce7fdcec0a2f021f6
Davis Tibbz <davisinyo@gmail.com>
2025-03-10 19:43:12 +0300

    add some repositories

---
+++ src/main/java/com/davistiba/wedemyserver/repository/SalesRepository.java
@@ -0,0 +1,23 @@
+package com.davistiba.wedemyserver.repository;
+
+import com.davistiba.wedemyserver.dto.SalesDTO;
+import com.davistiba.wedemyserver.models.Sales;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
+
+@Repository
+public interface SalesRepository extends CrudRepository<Sales, String> {
+
+    Slice<Sales> findByCreatedAtBetween(Instant createdAtStart, Instant createdAtEnd);
+
+    @Query("SELECT new com.davistiba.wedemyserver.dto.SalesDTO(s.transactionId,s.createdAt, s.paymentMethod, s.totalPaid, count(o)) " +
+            "FROM Sales s JOIN OrderItem o ON s.transactionId = o.sale.transactionId WHERE s.user.id = ?1 GROUP BY s.transactionId")
+    Slice<SalesDTO> findByUserIdOrderByCreatedAtDesc(Integer userId, Pageable pageable);
+
+
+}
