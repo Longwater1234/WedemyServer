e66184ef3ce6e8de79cd1d3c8ecbcd762483747c
Davis Tibbz <davisinyo@gmail.com>
2025-03-30 00:22:13 +0300

    remove equals and hashcode from EnrollmentDTO

--- src/main/java/com/davistiba/wedemyserver/dto/EnrollmentDTO.java
+++ src/main/java/com/davistiba/wedemyserver/dto/EnrollmentDTO.java
@@ -1,21 +1,18 @@
 package com.davistiba.wedemyserver.dto;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Getter
-@EqualsAndHashCode
+@NoArgsConstructor
 public class EnrollmentDTO {
     private Long id;
     private BigDecimal progress;
     private String title;
     private String thumbUrl;
     private Integer courseId;
-
-    public EnrollmentDTO() {
-    }
 
     public EnrollmentDTO(Long id, BigDecimal progress, String title, String thumbUrl, Integer courseId) {
         this.id = id;
